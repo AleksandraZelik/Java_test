4fb194c831dda67cac21f219e224631e293d2db8
Aleksandra Zelik <aleksandrazelik55@gmail.com>
2023-04-17 15:05:50 +0300

    14 homework

--- src/main/java/homework14/Main.java
+++ src/main/java/homework14/Main.java
@@ -1,12 +1,7 @@
 package homework14;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
