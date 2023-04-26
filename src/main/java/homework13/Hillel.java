a1dd0dd403114cc9392103ce708ec2f5a392a21e
Aleksandra Zelik <aleksandrazelik55@gmail.com>
2023-04-26 21:57:32 +0300

    13 homework changed

--- src/main/java/homework13/Hillel.java
+++ src/main/java/homework13/Hillel.java
@@ -1,8 +1,7 @@
 package homework13;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
 import java.util.List;
@@ -12,12 +11,13 @@ public class Hillel {
 
         WebDriver driver = new ChromeDriver();
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://ithillel.ua/");
 
-        WebElement programmingCourse = driver.findElement(By.xpath("//*[@id=\"body\"]/div[1]/main/section[2]/div/div/div[1]/div/ul/li[1]/a/div/p[2]"));
+        WebElement programmingCourse = driver.findElement(By.cssSelector(".course-cat-bar_icon[alt=\"Програмування\"]"));
         programmingCourse.click();
 
-        WebElement frontendBasicCourse = driver.findElement(By.xpath("//*[@id=\"categories\"]/div[3]/div/ul/li[1]/div/div[1]/ul/li[1]/a/div[2]"));
+        WebElement frontendBasicCourse = driver.findElement(By.cssSelector(".block-profession_link[href*='front-end-basic']"));
         frontendBasicCourse.click();
 
         WebElement lazySectionCoaches = driver.findElement(By.id("lazySectionCoaches"));
@@ -25,7 +25,6 @@ public class Hillel {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("arguments[0].scrollIntoView(true);", lazySectionCoaches);
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         WebElement button= driver.findElement(By.cssSelector("#coachesShowAllButton"));
         button.click();
